09ebc3832b03fbcb8972adc75df1e8f0e17437f3
Domingo1987 <domingoperez1987@gmail.com>
2023-04-17 15:00:52 -0300

    comm

--- procesos/main.c
+++ procesos/main.c
@@ -1,18 +1,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
  
 int main()
 {
     pid_t pid;
- 
-    if ( (pid=fork()) == 0 )
+
+ pid = fork();
+ sleep(1);
+
+    if (pid == 0) 
     { /* hijo */
         printf("Soy el hijo (%d, hijo de %d)\n", getpid(), getppid());
     }
     else
     { /* padre */
         printf("Soy el padre (%d, hijo de %d)\n", getpid(), getppid());
+        printf("Listado de procesos:\n");
+        system("ps -ef");
+        sleep(10); // Espera 10 segundos
     }
  
     return 0;
